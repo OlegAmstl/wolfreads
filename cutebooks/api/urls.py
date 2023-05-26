12da62dd5cc32607c1393f6b57bfa05a9a40d16d
Oleg <amstl@mail.ru>
2023-05-26 10:48:50 +0700

    В приложении api создан BookViewSet и роутер к нему.

--- cutebooks/api/urls.py
+++ cutebooks/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import IndexViewSet
+from .views import IndexViewSet, BookViewSet
 
 
 router_v1 = routers.DefaultRouter()
 router_v1.register('main', IndexViewSet, basename='index')
+router_v1.register('books', BookViewSet, basename='books')
 
 urlpatterns = [
     path('v1/', include(router_v1.urls))
--- cutebooks/api/views.py
+++ cutebooks/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework import mixins
 
 from books.models import Book
 from .serializers import BookSerializer
@@ -7,4 +8,9 @@ from .serializers import BookSerializer
 
 class IndexViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.order_by('-add_date')[:4]
+    serializer_class = BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
